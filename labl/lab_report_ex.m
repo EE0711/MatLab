0428f47070954832c7a1c36e29e6fa2c482cdd81
user <ElvisE0711@gmail.com>
2020-02-10 10:06:44 +0200

    2020_10_02

---
+++ labl/lab_report_ex.m
@@ -0,0 +1,19 @@
+%% example labors
+%% Mērijumu datu apstrade
+%% darba meerkji
+% * Iemaaciities apstraadaat meerijumu datus
+% * iemaaciities lietot polyfit, polyval
+% * iemaaciities veidot matlab atskaites izmantojot "publish"
+%% Darba programma:
+t = 0:0.01:1;
+y = sin(2*pi*t);
+plot(t,y)
+%%
+%
+% <<../grafiks1.jpg>>
+%
+%%
+%% Secinājumi
+% mees rakstiisim secinaajumus par to ko mes esam iemaacijushies shaja
+% darbaa
+% vai arii skatamies darba meerkji un rakstam secinaajumus
